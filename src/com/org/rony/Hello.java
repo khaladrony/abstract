2248fa0ace55ac64ac31363d79bfcfb6f3cb6ead
rony <rony@ubuntu.ubuntu-domain>
2018-11-23 23:56:14 +0600

    first git commit

---
+++ src/com/org/rony/Hello.java
@@ -0,0 +1,12 @@
+
+package com.org.rony;
+
+/**
+ *
+ * @author rony
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world...");
+    }
+}
